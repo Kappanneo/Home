46266bad237665c256a5b722cffa2f2918be72c6
Kappanneo <pannacciclaudio@gmail.com>
2018-02-04 19:04:03 +0100

    Better Faster Stronger.

--- bin/touchpad_toggle.sh
+++ bin/touchpad_toggle.sh
@@ -1,6 +1,7 @@
 if [ $(xinput list-props "FocalTechPS/2 FocalTech FocalTech Touchpad" | grep "Device Enabled" | cut -d ':' -f2) -eq "1" ];
 then
-xinput set-prop "FocalTechPS/2 FocalTech FocalTech Touchpad" "Device Enabled" 0
+    xinput set-prop "FocalTechPS/2 FocalTech FocalTech Touchpad" "Device Enabled" 0
+    xdotool mousemove 750 3000
 else
-xinput set-prop "FocalTechPS/2 FocalTech FocalTech Touchpad" "Device Enabled" 1
+    xinput set-prop "FocalTechPS/2 FocalTech FocalTech Touchpad" "Device Enabled" 1
 fi
