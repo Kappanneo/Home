e69ddcfb300c567b0b53fcb0aed6797dc7811ff4
Kappanneo <pannacciclaudio@gmail.com>
2018-05-27 11:40:05 +0200

    Smooth.

--- .bin/autoscroll.sh
+++ .bin/autoscroll.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+
 mouse=$(xinput list --name-only | grep "Mouse")
+
 xinput --set-prop "$mouse" 'Evdev Wheel Emulation' 1
 xinput --set-prop "$mouse" 'Evdev Wheel Emulation Button' 2
 xinput --set-prop "$mouse" 'Evdev Wheel Emulation Axes' 6 7 4 5
+xinput --set-prop 'Generic USB Mouse' 'Evdev Middle Button Timeout' 50
+xinput --set-prop 'Generic USB Mouse' 'Evdev Wheel Emulation Timeout' 100
