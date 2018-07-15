7abd5d2379c042d555c5f126bed1dce50d08e7a3
Kappanneo <pannacciclaudio@gmail.com>
2018-07-16 01:41:02 +0200

    Autoscroll speed fix.

--- .bin/autoscroll.sh
+++ .bin/autoscroll.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 mouse=$(xinput list --name-only | grep "Mouse")
 if [ $1 -eq "0" ]
 then
@@ -9,8 +8,8 @@ then
     xinput --set-prop "$mouse" 'Evdev Wheel Emulation' 1
     xinput --set-prop "$mouse" 'Evdev Wheel Emulation Button' 2
     xinput --set-prop "$mouse" 'Evdev Wheel Emulation Axes' 6 7 4 5
-    xinput --set-prop "$mouse" 'Evdev Middle Button Timeout' 50
-    xinput --set-prop "$mouse" 'Evdev Wheel Emulation Timeout' 100
+    xinput --set-prop "$mouse" 'Evdev Middle Button Timeout' 10
+    xinput --set-prop "$mouse" 'Evdev Wheel Emulation Timeout' 200
 elif [ $(xinput list-props "$mouse" | grep "Evdev Wheel Emulation (313)" | cut -d ':' -f2) -eq "1" ]
 then
     xinput --set-prop "$mouse" 'Evdev Wheel Emulation' 0
@@ -18,8 +17,8 @@ else
     xinput --set-prop "$mouse" 'Evdev Wheel Emulation' 1
     xinput --set-prop "$mouse" 'Evdev Wheel Emulation Button' 2
     xinput --set-prop "$mouse" 'Evdev Wheel Emulation Axes' 6 7 4 5
-    xinput --set-prop "$mouse" 'Evdev Middle Button Timeout' 50
-    xinput --set-prop "$mouse" 'Evdev Wheel Emulation Timeout' 100
+    xinput --set-prop "$mouse" 'Evdev Middle Button Timeout' 10
+    xinput --set-prop "$mouse" 'Evdev Wheel Emulation Timeout' 200
 fi
 
 
