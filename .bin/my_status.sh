debddc99fc93a8531ea938408df0cdef69659f55
Kappanneo <pannacciclaudio@gmail.com>
2019-01-21 00:47:09 +0100

    fixed wm no name

--- .bin/my_status.sh
+++ .bin/my_status.sh
@@ -3,6 +3,7 @@ i3status | while :
 do
     read line
     id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
-    name=$(xprop -len 30 -id $id | awk '/\<WM_NAME/{print}' | cut -d'"' -f2)
-    echo "$name | $line" || exit 1
+    name=$(xprop -len 30 -id $id | awk '/\<WM_NAME/{print}' | cut -d'=' -f2 | cut -d'"' -f2)
+    class=$(xprop -len 30 -id $id | awk '/\<WM_CLASS/{print}' | cut -d'=' -f2 | cut -d'"' -f2)
+    echo "$name | $class | $line" || exit 1
 done
