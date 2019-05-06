005c3305b230a725415ed2b9ebf9a31ee1407293
kappanneo <pannacciclaudio@gmail.com>
2019-05-06 21:07:14 +0200

    alt default to write mode

--- .i3/modes.py
+++ .i3/modes.py
@@ -23,7 +23,7 @@ MODES = {
 
 "$wrt":
     ('"WRITE: writing enabled"',
-     ['Return','BackSpace','space','Delete'],
+     ['Return','BackSpace','space','Delete','"Alt_L"'],
      "mode $def",
      "$exec $alert $wrt",
      "",
